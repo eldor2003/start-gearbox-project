89663bf33bceb5c1b2c261d695688c514676fa73
shahboz <suyuunov.shaxboz2704@gmail.com>
2024-03-23 22:52:51 +0500

    edit some mistakes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,142 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+	var phoneInputs = document.querySelectorAll('input[data-tel-input]');
+
+	var getInputNumbersValue = function (input) {
+
+		// Return stripped input value — just numbers
+
+		return input.value.replace(/\D/g, '');
+
+	}
+
+	var onPhonePaste = function (e) {
+
+		var input = e.target,
+
+			inputNumbersValue = getInputNumbersValue(input);
+
+		var pasted = e.clipboardData || window.clipboardData;
+
+		if (pasted) {
+
+			var pastedText = pasted.getData('Text');
+
+			if (/\D/g.test(pastedText)) {
+
+				// Attempt to paste non-numeric symbol — remove all non-numeric symbols,
+
+				// formatting will be in onPhoneInput handler
+
+				input.value = inputNumbersValue;
+
+				return;
+
+			}
+
+		}
+
+	}
+
+	var onPhoneInput = function (e) {
+
+		var input = e.target,
+
+			inputNumbersValue = getInputNumbersValue(input),
+
+			selectionStart = input.selectionStart,
+
+			formattedInputValue = "";
+
+		if (!inputNumbersValue) {
+
+			return input.value = "";
+
+		}
+
+		if (input.value.length != selectionStart) {
+
+			// Editing in the middle of input, not last symbol
+
+			if (e.data && /\D/g.test(e.data)) {
+
+				// Attempt to input non-numeric symbol
+
+				input.value = inputNumbersValue;
+
+			}
+
+			return;
+
+		}
+
+		if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+
+			if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+
+			var firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+
+			formattedInputValue = input.value = firstSymbols + " ";
+
+			if (inputNumbersValue.length > 1) {
+
+				formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+
+			}
+
+			if (inputNumbersValue.length >= 5) {
+
+				formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+
+			}
+
+			if (inputNumbersValue.length >= 8) {
+
+				formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+
+			}
+
+			if (inputNumbersValue.length >= 10) {
+
+				formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+
+			}
+
+		} else {
+
+			formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
+
+		}
+
+		input.value = formattedInputValue;
+
+	}
+
+	var onPhoneKeyDown = function (e) {
+
+		// Clear input after remove last symbol
+
+		var inputValue = e.target.value.replace(/\D/g, '');
+
+		if (e.keyCode == 8 && inputValue.length == 1) {
+
+			e.target.value = "";
+
+		}
+
+	}
+
+	for (var phoneInput of phoneInputs) {
+
+		phoneInput.addEventListener('keydown', onPhoneKeyDown);
+
+		phoneInput.addEventListener('input', onPhoneInput, false);
+
+		phoneInput.addEventListener('paste', onPhonePaste, false);
+
+	}
+
+})
 $(document).ready(function () {
 
 	// quiz opener
@@ -272,6 +411,7 @@ $(document).ready(function () {
 
 	// tel mask
 	const element = document.querySelectorAll("input[type='tel']");
+	
 	element.forEach((item) => {
 		const maskOptions = {
 			mask: "+{0} 000-000-0000",
@@ -420,4 +560,19 @@ $(document).ready(function () {
 		$('.services_list:first-child').slideToggle(300)
 	})
 	
+	$('.help img:not(:first-child)').css('opacity','0')
+	arr = $('.help img')
+	i=1
+	
+	setInterval(function() {
+		$('.help img').css('opacity','0')
+		$(arr[i]).css('opacity', '1')
+		i++
+		if(i==4){
+			i=0
+		}
+	}, 10000);
+
+	
+
 });
